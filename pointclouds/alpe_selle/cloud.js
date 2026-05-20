3b53ed6da1307a880cbb4f22edc175fff2b216d3
geotervb <stefano.baldissera@geoter.verbania.it>
2026-05-20 12:02:12 +0200

    Upload Potree Alpe Selle clean

---
+++ pointclouds/alpe_selle/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 12120993,
+    "boundingBox": {
+        "lx": 430140.3139953613,
+        "ly": 5086824.218269348,
+        "lz": 1809.0859375,
+        "ux": 430360.3794708252,
+        "uy": 5087044.283744812,
+        "uz": 2029.1514129638672
+    },
+    "tightBoundingBox": {
+        "lx": 430140.314,
+        "ly": 5086824.218,
+        "lz": 1809.086,
+        "ux": 430360.379,
+        "uy": 5086973.779,
+        "uz": 1839.807
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.9058228731155396,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
